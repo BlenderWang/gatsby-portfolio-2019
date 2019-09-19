f45b05b1d591ba8bacf443ba9ba5936d5c603f3d
Shirley Wang <shirleywang527@gmail.com>
2019-09-19 18:51:08 +0200

    housekeeping

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,9 +9,6 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
-import Nav from "./nav"
-import About from "./about"
 import Social from "./social"
 import "./layout.scss"
 
@@ -28,14 +25,10 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <Nav />
-      <About />
       <div
         style={{
           margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
+          maxWidth: `100vw`,
           paddingTop: 0,
         }}
       >
--- src/components/nav.js
+++ src/components/nav.js
@@ -11,7 +11,7 @@ const Nav = ({ siteTitle }) => (
 
         <ul id="menu">
             <li className='home-section'><Link to='/'>Home</Link></li>
-            <li className='about-section'><Link to='/'>About Me</Link></li>
+            <li className='about-section'><Link to='#about'>About Me</Link></li>
             <li className='skills-section'><Link to='/skills'>Skills</Link></li>
             <li className='portfolio-section'><Link to='/portfolio'>Portfolio</Link></li>
             <li className='contact-section'><Link to='/contact'>Contact Me</Link></li>
