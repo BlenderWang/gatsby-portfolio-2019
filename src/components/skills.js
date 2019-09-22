bb3570ccb9cf11d1e66811cf18c5cdb3dd8b7d67
Shirley Wang <shirleywang527@gmail.com>
2019-09-22 20:52:27 +0200

    cursor hover over folders

--- src/components/skills.js
+++ src/components/skills.js
@@ -1,7 +1,8 @@
 import React from "react"
 import { Link } from "gatsby"
 import skillsImg from "../images/skills-img.svg"
-import folderDummy from "../images/folder-dummy.svg"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 import file01 from "../images/file_1.svg"
 import file02 from "../images/file_2.svg"
 import file03 from "../images/file_3.svg"
@@ -29,7 +30,14 @@ const Skills = () => {
                             <img src={file03} alt="JS skills" className="folder__thumb"/>
                         </div>
                         <div className="folder__icon">
-                            <img src={folderDummy} alt=""/>
+                            <FontAwesomeIcon icon={['fas', 'code']} className="fa-icons fa-code" />
+                            <svg id="Layer_1" x="0px" y="0px" viewBox="0 0 20 16">
+                                <g>
+                                    <path className="st0" d="M7.5,0C7.4,0,2,0,2,0C0.9,0,0,0.9,0,2l0,12c0,1.1,0.9,2,2,2h16c1.1,0,2-0.9,2-2V4c0-1.1-0.9-2-2-2
+                                        c0,0-7.5,0-8,0C9,2,9.9,0,7.5,0z"/>
+                                    <path className="st1" d="M0,2l18,0c1.1,0,2,0.9,2,2v10c0,1.1-0.9,2-2,2H2c-1.1,0-2-0.9-2-2L0,2z"/>
+                                </g>
+                            </svg>
                         </div>
                         <h3 className="folder__caption">Programming</h3>
                     </div>
@@ -44,7 +52,14 @@ const Skills = () => {
                             <img src={file03} alt="JS skills" className="folder__thumb"/>
                         </div>
                         <div className="folder__icon">
-                            <img src={folderDummy} alt=""/>
+                            <FontAwesomeIcon icon={['fas', 'image']} className="fa-icons fa-image" />
+                            <svg id="Layer_1" x="0px" y="0px" viewBox="0 0 20 16">
+                                <g>
+                                    <path className="st0" d="M7.5,0C7.4,0,2,0,2,0C0.9,0,0,0.9,0,2l0,12c0,1.1,0.9,2,2,2h16c1.1,0,2-0.9,2-2V4c0-1.1-0.9-2-2-2
+                                        c0,0-7.5,0-8,0C9,2,9.9,0,7.5,0z"/>
+                                    <path className="st1" d="M0,2l18,0c1.1,0,2,0.9,2,2v10c0,1.1-0.9,2-2,2H2c-1.1,0-2-0.9-2-2L0,2z"/>
+                                </g>
+                            </svg>
                         </div>
                         <h3 className="folder__caption">Graphics</h3>
                     </div>
--- src/pages/page-skills.js
+++ src/pages/page-skills.js
@@ -4,6 +4,12 @@ import Layout from "../components/layout"
 import Nav from "../components/nav"
 import Skills from "../components/skills"
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas }  from '@fortawesome/free-solid-svg-icons'
+// import { fab }  from '@fortawesome/free-brands-svg-icons'
+
+library.add(fas)
+
 const SkillsPage = () => (
   <Layout>
     <Nav/>
